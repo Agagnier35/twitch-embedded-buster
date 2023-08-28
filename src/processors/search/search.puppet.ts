8f9c485d3ca9a9a10d2670cc9b1cbf450524dfef
Felix Agagnier <felix_agagnier@hotmail.ca>
2023-08-28 09:04:56 -0400

    Add third page links

--- src/processors/search/search.puppet.ts
+++ src/processors/search/search.puppet.ts
@@ -25,7 +25,6 @@ export const searchWikis = async (
 
   // Input Search Query
   const engine = SearchEngines[searchEngine];
-
   const blackListQueryString = BlacklistedWebsites.map(
     (s) => engine.minusSitePrefix + s
   ).join(" ");
@@ -34,37 +33,42 @@ export const searchWikis = async (
     `${game.name} ${keyword} ${blackListQueryString}`
   );
 
-  await Promise.all([
-    await page.keyboard.press("Enter"),
+  await page.keyboard.press("Enter"),
+    console.log(
+      `Scrapping First Page search results for game ${game.name} (${keyword})...`
+    );
+  const scraps1 = await scrapePage(page, searchEngine);
 
-    // Scrape Page 1
-    await page.waitForLoadState("domcontentloaded"),
-    await page.waitForSelector(engine.linkSelector),
-  ]);
   console.log(
-    `Scrapping First Page search results for game ${game.name} (${keyword})...`
+    `Scrapping Second Page search results for game ${game.name} (${keyword})...`
   );
+  const scraps2 = await scrapePage(page, searchEngine);
 
-  const scraps1 = await page.$$eval(engine.linkSelector, async (els) =>
-    (els as HTMLElement[])
-      .filter((e) => e.parentElement?.childElementCount === 1)
-      .map((e) => ({
-        title: e.innerText,
-        link: (e as HTMLAnchorElement).href,
-      }))
+  console.log(
+    `Scrapping Thirsd Page search results for game ${game.name} (${keyword})...`
   );
+  const scraps3 = await scrapePage(page, searchEngine);
 
-  //Navigate Page 2
+  console.log(`Links scrapped for ${game.name} (${keyword})`);
+  await page.close();
+  return [...scraps1, ...scraps2, ...scraps3].map((link) => ({
+    ...link,
+    game,
+    keyword,
+  }));
+};
+
+const scrapePage = async (page: Page, searchEngine: SearchEngine) => {
+  const engine = SearchEngines[searchEngine];
+
+  //Navigate Page 3
   await Promise.all([
     await page.click(engine.pageSelector),
     await page.waitForLoadState("domcontentloaded"),
   ]);
-  console.log(
-    `Scrapping Second Page search results for game ${game.name} (${keyword})...`
-  );
 
-  // Scrape Page 2
-  const scraps2 = await page.$$eval(engine.linkSelector, async (els) =>
+  // Scrape Page 3
+  return await page.$$eval(engine.linkSelector, async (els) =>
     (els as HTMLElement[])
       .filter((e) => e.parentElement?.childElementCount === 1)
       .map((e) => ({
@@ -72,8 +76,4 @@ export const searchWikis = async (
         link: (e as HTMLAnchorElement).href,
       }))
   );
-
-  console.log(`Links scrapped for ${game.name} (${keyword})`);
-  await page.close();
-  return [...scraps1, ...scraps2].map((link) => ({ ...link, game, keyword }));
 };
